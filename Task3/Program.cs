c783107a62c8e04b7bb9628d38c9dc9cd09150d1
zelenka <eva.zelya@gmail.com>
2024-05-18 14:44:37 +0300

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+int[] CreatRndArr(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void ReverseArray(int [] array, int i)
+{
+    if(i < array.Length)
+    {
+        Console.Write(array[array.Length - i - 1] + " ");
+        ReverseArray(array, i + 1);
+    }
+}
+
+Console.WriteLine("input array size");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input min range");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input max range");
+int max = Convert.ToInt32(Console.ReadLine());
+int[] array = CreatRndArr(size, min, max);
+PrintArray(array);
+ReverseArray(array, 0);
